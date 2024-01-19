8d6718f9c86a26a9420fd13ff688bd67acc8ea43
Thomaz Rosseti <tocorosseti@gmail.com>
2024-01-19 15:26:27 -0300

    feat: logo image fix

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom"
 import { ErrorPage } from "./pages/ErrorPage"
 import { AboutPage } from "./pages/AboutPage"
+import { HomePage } from "./pages/HomePage"
 
 export const RouteMain = () => {
     return (
         <Routes>
+            <Route path="/" element={<HomePage/> } />
             <Route path="/about" element={<AboutPage />} />
             <Route path="*" element={<ErrorPage />} />
         </Routes>   
--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,6 +1,8 @@
 export const Footer = () => {
     return (
-        <>
-        </>
+        <footer>
+          <h3>React Aplication</h3>
+          <p><span>PokeReactApi</span> &copy; 2024</p>
+        </footer>
     )
 }
