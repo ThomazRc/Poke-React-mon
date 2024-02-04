53d9a3145c5dea726c82827475eb676291068e40
Thomaz Rosseti <tocorosseti@gmail.com>
2024-02-04 17:55:49 -0300

    feat: dinamical link and developement of AboutPage

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,18 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { StyledHeader } from "./style";
 
 export const Header = () => {
+  const location = useLocation();
+
+  const linkTo = location.pathname === "/about" ? "/" : "/about";
+  const linkText = location.pathname === "/about" ? "Home" : "About";
+  
   return (
     <StyledHeader>
       <nav>
       <img src="/src/assets/pokeapi-removebg-preview.png" alt="Logo PokeReactApi"/>
       <ul>
-        <li><Link to={"/about"}>About</Link></li>
+        <li><Link to={linkTo}>{linkText}</Link></li>
       </ul>
     </nav>
     </StyledHeader>
--- src/pages/aboutPage/index.tsx
+++ src/pages/aboutPage/index.tsx
@@ -1,8 +1,15 @@
+import { Footer } from "../../components/Footer";
+import { Header } from "../../components/Header";
+
 export const AboutPage = () => {
   return (
     <>
+      <Header />
+      <main>
       <h1>Contando mais sobre o projeto</h1>
       <p>Inserir texto de explicação.</p>
+      </main>
+      <Footer />
     </>
   );
 };
