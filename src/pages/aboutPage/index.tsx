077b44105d0a686a11159354cc16fdaa63f40fa9
Thomaz Rosseti <tocorosseti@gmail.com>
2024-02-12 14:37:44 -0300

    feat: new libraries

--- src/pages/aboutPage/index.tsx
+++ src/pages/aboutPage/index.tsx
@@ -1,15 +1,8 @@
-import { useState } from "react";
 import { Footer } from "../../components/Footer";
 import { Header } from "../../components/Header";
 import { AboutMain } from "./style";
 
 export const AboutPage = () => {
-  const [language, setLanguage] = useState<"pt" | "en">("pt");
-
-  const changeLanguage = () => {
-    setLanguage(language === "pt" ? "en" : "pt");
-  };
-
 
   return (
     <>
